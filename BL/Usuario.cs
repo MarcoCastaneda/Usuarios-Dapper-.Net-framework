9259aabb5c0f12b603d34bc7acbfbdda6882b314
MarcoCastaneda <m.castaneda.bautista01@gmail.com>
2022-08-12 12:47:50 -0500

    Agregar archivos de proyecto.

---
+++ BL/Usuario.cs
@@ -0,0 +1,203 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Usuario
+    {
+
+
+        public static ML.Result GetAll()
+
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+
+                using (var context = new SqlConnection(DL.Conexion.GetConnection()))
+                {
+                    //var sql = "UsuarioGetAll";
+                    var lst = context.Query<ML.Usuario>("UsuarioGetAll").ToList(); //Dapper
+
+                    result.Objects = new List<object>();
+                    foreach (var obj in lst)
+                    {
+
+                        ML.Usuario usuario = new ML.Usuario();
+
+                        usuario.IdUsuario = obj.IdUsuario;
+                        usuario.Nombre = obj.Nombre;
+                        usuario.ApellidoPaterno = obj.ApellidoPaterno;
+                        usuario.ApellidoMaterno = obj.ApellidoMaterno;
+
+                        result.Objects.Add(usuario);
+                    }
+                    result.Correct = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMensage = ex.Message;
+            }
+
+            return result;
+        }
+
+
+
+        public static ML.Result GetById(int IdUsuario)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (var context = new SqlConnection(DL.Conexion.GetConnection()))
+                {
+                    var lst = context.Query<ML.Usuario>($"UsuarioGetById {IdUsuario}").AsEnumerable().FirstOrDefault();
+
+                    result.Objects = new List<object>();
+
+                    if (lst != null)
+                    {
+                        ML.Usuario usuario = new ML.Usuario();
+
+                        usuario.IdUsuario = lst.IdUsuario;
+                        usuario.Nombre = lst.Nombre;
+                        usuario.ApellidoPaterno = lst.ApellidoPaterno;
+                        usuario.ApellidoMaterno = lst.ApellidoMaterno;
+
+                        result.Object = usuario;
+                        result.Correct = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMensage = ex.Message;
+            }
+            return result;
+        }
+
+
+        public static ML.Result Add(ML.Usuario usuario)
+
+        {
+
+            ML.Result result = new ML.Result();
+            try { 
+
+            using (var context = new SqlConnection(DL.Conexion.GetConnection()))
+            {
+
+                    //Utiliza Dapper en Execute
+                    var lst = context.Execute($"UsuarioAdd '{usuario.Nombre}','{usuario.ApellidoMaterno}','{usuario.ApellidoPaterno}'");
+
+                    if (lst > 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMensage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+
+
+
+        public static ML.Result Update(ML.Usuario usuario)
+
+        {
+
+            ML.Result result = new ML.Result();
+            try
+            {
+
+                using (var context = new SqlConnection(DL.Conexion.GetConnection()))
+                {
+
+
+                    var lst = context.Execute($"UsuarioUpdate {usuario.IdUsuario},'{usuario.Nombre}','{usuario.ApellidoMaterno}','{usuario.ApellidoPaterno}'");//Utiliza Dapper
+
+                    if (lst > 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMensage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+
+
+
+        public static ML.Result Delete(ML.Usuario usuario)
+
+        {
+
+            ML.Result result = new ML.Result();
+            try
+            {
+
+                using (var context = new SqlConnection(DL.Conexion.GetConnection()))
+                {
+
+
+                    var lst = context.Execute($"UsuarioDelete {usuario.IdUsuario}");//Utiliza Dapper
+
+                    if (lst > 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMensage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+
+
+
+    }
+
+    
+
+
+        
+            
+    
+}
---
+++ PL/Controllers/UsuarioController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            ML.Usuario usuario = new ML.Usuario();
+
+            ML.Result result = BL.Usuario.GetAll();
+
+            usuario.Usuarios = result.Objects;
+
+
+            return View(usuario);
+        }
+
+
+
+        [HttpGet]
+        public ActionResult Form(int? IdUsuario)
+        {
+
+            ML.Usuario usuario = new ML.Usuario();
+            if (IdUsuario == null)
+            {
+                return View(usuario);
+            }
+            else
+            {
+                ML.Result result = new ML.Result();
+                result = BL.Usuario.GetById(IdUsuario.Value);
+
+                if (result.Correct)
+                {
+                    usuario = ((ML.Usuario)result.Object);
+                }
+
+            }
+            return View(usuario);
+        }
+
+
+        [HttpPost]
+        public ActionResult Form(ML.Usuario usuario)
+        {
+            ML.Result result = new ML.Result();
+
+
+
+
+            if (usuario.IdUsuario == 0)
+            {
+
+                result = BL.Usuario.Add(usuario);
+
+                if (result.Correct)
+                {
+
+
+
+
+
+
+                    ViewBag.Mensaje = "El Libro se ha agregado";
+                }
+                else
+                {
+                    ViewBag.Mensaje = "El Libro no se ha agregado";
+                }
+            }
+            else
+            {
+                result = BL.Usuario.Update(usuario);
+
+                if (result.Correct)
+                {
+                    ViewBag.Mensaje = "El Libro se actualizo correctamente";
+                }
+                else
+                {
+                    ViewBag.Mensaje = "Ocurrio un error al realizar la actualizacion" + result.ErrorMensage;
+                }
+            }
+
+
+
+            return PartialView("Modal");
+        }
+
+
+        [HttpGet]
+        public ActionResult Delete(ML.Usuario usuario)
+        {
+            ML.Result result = BL.Usuario.Delete(usuario);
+
+            if (result.Correct)
+            {
+                ViewBag.Mensaje = "El libro se ha eliminado";
+            }
+            else
+            {
+                ViewBag.Mensaje = "error al eliminar";
+            }
+            return PartialView("modal");
+        }
+
+
+
+
+
+
+    }
+}
